3f032b3219b32f83ca5e7a51f4a806fafaa4d2be
RamBoFe <rambof@gmail.com>
2019-06-06 14:19:07 +0200

    Added feature to download torrent with transmission client + alert.

--- src/app/services/api/torrents/ygg.service.ts
+++ src/app/services/api/torrents/ygg.service.ts
@@ -1,18 +1,26 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
+const URL = 'http://localhost:2223/torrents/ygg';
 
 @Injectable()
 export class YggService {
 
   constructor(private http: HttpClient) {}
 
-  async getSearch(searched): Promise<any> {
+  async getSearch(search: string): Promise<any> {
     return this.http
-      .get<Array<any>>('http://localhost:2223/torrents/ygg/search', {
-        params: {search: searched.search}
+      .get<Array<any>>(`${URL}/search`, {
+        params: { search }
       })
       .toPromise();
   }
 
+  async addTorrentToDl(torrent: object): Promise<any> {
+    return this.http
+      .get<Object>(`${URL}/dl`, {
+        params: { torrent: JSON.stringify(torrent) }
+      })
+      .toPromise();
+  }
 }
