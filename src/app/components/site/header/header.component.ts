a7ead981baac13888d64c04b71740ee9c94366b1
RamBoFe <rambof@gmail.com>
2024-02-13 00:10:32 +0100

    Work in progress...

--- src/app/components/site/header/header.component.ts
+++ src/app/components/site/header/header.component.ts
@@ -1,36 +1,18 @@
 import {Component, OnInit} from '@angular/core';
-import {initializeApp} from 'firebase/app';
-import {getAuth, GoogleAuthProvider, signInWithPopup} from 'firebase/auth';
+import {FirebaseService} from '../../../services/firebase.service';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
-  constructor() {}
+  constructor(private readonly firebaseSrv: FirebaseService) {}
 
   ngOnInit() {}
 
   async signIn() {
-    // Your web app's Firebase configuration
-    const firebaseConfig = {
-      apiKey: 'AIzaSyC1JaeAXR1CcmoDYfFzELuVkkoNUlKOjPo',
-      authDomain: 'torramt-831ee.firebaseapp.com',
-      projectId: 'torramt-831ee',
-      storageBucket: 'torramt-831ee.appspot.com',
-      messagingSenderId: '528944375172',
-      appId: '1:528944375172:web:792d0ef57c78a2972be9d1',
-    };
-
-    // Initialize Firebase
-    const app = initializeApp(firebaseConfig);
-    const provider = new GoogleAuthProvider();
     try {
-      const auth = getAuth();
-      const result = await signInWithPopup(auth, provider);
-
-      // Pull signed-in user credential.
-      const user = result.user;
+      const user = await this.firebaseSrv.signInWithPopup();
       console.log(user);
     } catch (e) {
       console.log(e);
---
+++ src/app/services/firebase.service.ts
@@ -0,0 +1,36 @@
+import {Injectable} from '@angular/core';
+import {initializeApp} from 'firebase/app';
+import {
+  getAuth,
+  GoogleAuthProvider,
+  signInWithPopup,
+  User,
+} from 'firebase/auth';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class FirebaseService {
+  private firebaseConfig = {
+    apiKey: 'AIzaSyC1JaeAXR1CcmoDYfFzELuVkkoNUlKOjPo',
+    authDomain: 'torramt-831ee.firebaseapp.com',
+    projectId: 'torramt-831ee',
+    storageBucket: 'torramt-831ee.appspot.com',
+    messagingSenderId: '528944375172',
+    appId: '1:528944375172:web:792d0ef57c78a2972be9d1',
+  };
+  constructor() {
+    initializeApp(this.firebaseConfig);
+  }
+
+  /**
+   *
+   */
+  async signInWithPopup(): Promise<User> {
+    const userCredential = await signInWithPopup(
+      getAuth(),
+      new GoogleAuthProvider()
+    );
+    return userCredential.user;
+  }
+}
