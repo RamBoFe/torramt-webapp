eec76895bb2c3210cd9dbee5dc2fbc2431d36216
RamBoFe <rambof@gmail.com>
2024-02-28 01:10:20 +0100

    Working in progress...

--- src/app/administration/administration.module.ts
+++ src/app/administration/administration.module.ts
@@ -1,10 +1,26 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
 import { routes } from './administration.routing';
 import { SettingsComponent } from './components/settings/settings.component';
 
 @NgModule({
   declarations: [SettingsComponent],
-  imports: [CommonModule, routes],
+  imports: [
+    CommonModule,
+    routes,
+    MatFormFieldModule,
+    FormsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    MatSelectModule,
+  ],
 })
 export class AdministrationModule {}
--- src/app/administration/components/settings/settings.component.ts
+++ src/app/administration/components/settings/settings.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-settings',
   templateUrl: './settings.component.html',
-  styleUrls: ['./settings.component.scss']
+  styleUrls: ['./settings.component.scss'],
 })
 export class SettingsComponent implements OnInit {
+  form = new FormGroup({
+    protocol: new FormControl('https', [Validators.required]),
+    host: new FormControl('', [Validators.required]),
+    port: new FormControl('', [Validators.required]),
+    login: new FormControl('', [Validators.required]),
+    password: new FormControl('', [Validators.required]),
+    tag: new FormControl('', [Validators.required]),
+  });
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
